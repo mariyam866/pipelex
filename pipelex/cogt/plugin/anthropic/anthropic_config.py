a26a3946c3019a8a9375564b54ad32bb549cc6fe
Thomas Hebrard <thomas@pipelex.com>
2025-05-28 10:31:27 +0200

    Feature/pipelex init base config (#6)
    * add
    
    * make cc
    
    * better error handling
    
    * pipelex_init
    
    * click exceptions
    
    * Hardode API key variable names
    
    * remove pipelex.toml
    
    * custom_endpoint_config
    
    * fix CUSTOM_ENDPOINT_BASE_URL in .env.example
    
    * make fix-unused-imports
    
    * remove pipelex.toml at the root
    
    * change gitignore
    
    * bump typer
    
    * Fix/pipelex libraries module (#7)
    
    * fix/pipelex_libraries-module
    
    * update bootstrap
    
    * make cc
    
    * test without de init
    
    * test with no bootstrap
    
    * fix

--- pipelex/cogt/plugin/anthropic/anthropic_config.py
+++ pipelex/cogt/plugin/anthropic/anthropic_config.py
@@ -19,24 +19,28 @@ class AnthropicKeyMethod(StrEnum):
     ENV = "env"
 
 
+ANTHROPIC_API_KEY_VAR_NAME = "ANTHROPIC_API_KEY"
+
+
 class AnthropicConfig(ConfigModel):
     api_key_method: AnthropicKeyMethod = Field(strict=False)
-    api_key_secret_name: str
-    api_key_env_var_name: str
+
+    def configure(self, secrets_provider: SecretsProviderAbstract) -> str:
+        return self.get_api_key(secrets_provider=secrets_provider)
 
     def get_api_key(self, secrets_provider: SecretsProviderAbstract) -> str:
         match self.api_key_method:
             case AnthropicKeyMethod.ENV:
                 log.verbose("Getting Anthropic API key from environment.")
                 try:
-                    key_from_env = get_required_env(self.api_key_env_var_name)
+                    key_from_env = get_required_env(ANTHROPIC_API_KEY_VAR_NAME)
                     return key_from_env
                 except EnvVarNotFoundError as exc:
                     raise AnthropicCredentialsError(f"Error getting Anthropic API key from environment: {exc}") from exc
             case AnthropicKeyMethod.SECRET_PROVIDER:
                 log.verbose("Getting Anthropic API key from secrets provider.")
                 try:
-                    key_from_service_provider = secrets_provider.get_secret(secret_id=self.api_key_secret_name)
+                    key_from_service_provider = secrets_provider.get_secret(secret_id=ANTHROPIC_API_KEY_VAR_NAME)
                 except SecretNotFoundError as exc:
                     raise AnthropicCredentialsError("Error getting Anthropic API key from secrets provider.") from exc
                 return key_from_service_provider
